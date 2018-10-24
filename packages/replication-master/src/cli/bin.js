2d7e11109d1bb6f911be063130dce83a57f0fbef
achingbrain <alex@achingbrain.net>
2018-10-24 07:50:25 +0100

    perf: reduce concurrency

--- packages/replication-master/src/cli/bin.js
+++ packages/replication-master/src/cli/bin.js
@@ -114,7 +114,7 @@ yargs.command('$0', 'Starts a registry server that uses IPFS to fetch js depende
     })
     .option('follow-concurrency', {
       describe: 'How many registry updates to process at once',
-      default: 50
+      default: 10
     })
     .option('follow-seq-file', {
       describe: 'Where to store the seq file of how far through the npm feed we are',
