259b7b89e60e29ba210ce721d183aeec41b4403f
achingbrain <alex@achingbrain.net>
2018-11-29 11:40:07 +0000

    perf: only request the hash of the base dir

--- packages/common/utils/find-base-dir.js
+++ packages/common/utils/find-base-dir.js
@@ -2,12 +2,15 @@
 
 const findBaseDir = async (config, ipfs) => {
   try {
-    const stats = await ipfs.files.stat(config.ipfs.prefix)
+    const stats = await ipfs.files.stat(config.ipfs.prefix, {
+      hash: true
+    })
 
     return stats.hash
   } catch (error) {
     if (error.message.includes('does not exist')) {
       console.info('🐺 Creating base dir') // eslint-disable-line no-console
+
       await ipfs.files.mkdir(config.ipfs.prefix, {
         parents: true
       })
