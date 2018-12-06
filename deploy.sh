1442e0caf8ebb910a8ade81ab3d0b3476e8ab217
achingbrain <alex@achingbrain.net>
2018-12-06 12:41:00 +0000

    chore: prune unused images

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 
 # Remove old images
+docker system prune -a -f
 docker rm $(docker ps -q -f 'status=exited') || echo 'Failed to remove old containers, maybe there was nothing to do'
 docker rmi $(docker images -q -f "dangling=true") || echo 'Failed to remove old images, maybe there was nothing to do'
 
