2b9cf8faffd56851c234dce5663b8d385291d74e
achingbrain <alex@achingbrain.net>
2019-02-14 16:59:24 +0000

    chore: add monitoring

--- upgrade.sh
+++ upgrade.sh
@@ -5,8 +5,14 @@ docker system prune -a -f
 docker rm $(docker ps -q -f 'status=exited') || echo 'Failed to remove old containers, maybe there was nothing to do'
 docker rmi $(docker images -q -f "dangling=true") || echo 'Failed to remove old images, maybe there was nothing to do'
 
+# Get the latest
+git pull
+
 # Build a Docker image
-docker-compose build --no-cache
+docker-compose build --no-cache replicate registry
+
+# Shut down the registry containers
+docker-compose stop replicate registry
 
 # Restart using the new image
-docker-compose up -d --scale registry=5
+docker-compose up -d --no-deps --scale registry=5 replicate registry
